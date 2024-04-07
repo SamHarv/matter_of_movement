592ee2f81c1e9a53c1a4d91c51a426c240931be9
SamHarv <srharvey93@gmail.com>
2024-04-08 06:03:57 +1000

    New draft post

--- lib/post_data.dart
+++ lib/post_data.dart
@@ -2,6 +2,41 @@ import 'models/post_model.dart';
 
 List<Post> postData = [
   Post(
+    title: 'Choose Cognitive Inputs Wisely',
+    datePosted: DateTime(2024, 4, 12),
+    image: 'images/1.png',
+    id: '121',
+    body: 'Misinformation is more rampant, and more convincing than ever.\n\n'
+        'Media outlets continue to push their agenda with the goal of keeping people '
+        'viewing or reading their content.\n\n'
+        'They dramatise events with imagery and prose, often leading us to believe '
+        'the world is a more dangerous place than it is.\n\n'
+        'Social media is prone to containing more misinformation than truth.\n\n'
+        'With the rise of generative artificial intelligence, it has never been easier '
+        'to create fake news.\n\n'
+        'All this information can lead us to believe that what we see is all there '
+        'is. This is an example of availability bias, where we believe what we can '
+        'most easily recall.\n\n'
+        'What we can easily recall is what we constantly see, and in this day and '
+        'age, is often misinformation.\n\n'
+        'We should choose to subscribe to newsletters that report on positive news, '
+        'which will give us a more positive outlook on the world.\n\n'
+        'If we see something on social media, or even on the news, we should '
+        'question its validity.\n\n'
+        'Consider why what is being reported might be designed to elicit an '
+        'emotional response rather than inform.\n\n'
+        'We should seek information from peer-reviewed journals, from books written '
+        'by authors whose values align with our own, and from sources '
+        'which are not incentivised to push an agenda.\n\n'
+        'Perhaps the most beneficial thing we can do is to limit our exposure to '
+        'misinformation and negatively skewed news is to limit our time on social '
+        'media.\n\n'
+        'Turn off the notifications for social media apps on your smart phone and '
+        'check them when it suits you.\n\n'
+        'If we can control our cognitive inputs, we can be less susceptible to '
+        'misnformation and manipulation, and we can be happier and healthier as a result.',
+  ),
+  Post(
     title: 'Cold Water Immersion',
     datePosted: DateTime(2024, 4, 5),
     image: 'images/1.png',
