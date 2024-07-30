4879efe1014268607bb1c31ce495175f24f8db26
SamHarv <srharvey93@gmail.com>
2024-07-30 15:34:38 +1000

    redraft

--- lib/post_data.dart
+++ lib/post_data.dart
@@ -6,7 +6,12 @@ List<Post> postData = [
     datePosted: DateTime(2024, 8, 2),
     image: 'images/1.png',
     id: '137',
-    body: 'Focus on your long-term aspirations, but be willing to make '
+    body: 'When choosing how to spend our time, we can often be met with a '
+        'decision between what benefits us in the short-term, or what benefits us '
+        'in the long-term.\n\n'
+        'It is a trade-off between instant gratification and delayed '
+        'gratification.\n\n'
+        'Focus on your long-term aspirations, but be willing to make '
         'adjustments in ther short-term to get there.',
   ),
   Post(
