255cf80ca3ed2bd5261a305c29ec4d1a1b405e31
Sam Harvey <srharvey93@gmail.com>
2025-01-30 09:31:18 +1100

    Final draft

--- lib/logic/services/url_launcher.dart
+++ lib/logic/services/url_launcher.dart
@@ -2,7 +2,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 class UrlLauncher {
   /// Launch O2Tech website
-  final _url = Uri.parse('https://oxygentech.com.au');
+  final _url = Uri.parse('https://o2tech.com.au');
 
   Future<void> launchO2Tech() async {
     if (!await launchUrl(_url)) {
